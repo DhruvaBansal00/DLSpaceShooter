3b75492c75991eac3b894ff73c353f8a77b91d21
DhruvaBansal00 <dhruva.bansal00@gmail.com>
2018-07-14 02:07:21 +0530

    WIP: biolerplate DQN Code

--- DQNCode.py
+++ DQNCode.py
@@ -6,7 +6,16 @@ import numpy
 import MLModifiedSpaceShooter as game
 from collections import deque
 
-ACTIONS = 3  # right, left, shoot
+GAME = 'spaceShooter' # the name of the game being played for log files
+ACTIONS = 3 # number of valid actions
+GAMMA = 0.95 # decay rate of past observations changed temporarily
+OBSERVE = 100000. # timesteps to observe before training
+EXPLORE = 2000000. # frames over which to anneal epsilon
+FINAL_EPSILON = 0.0001 # final value of epsilon
+INITIAL_EPSILON = 1 # starting value of epsilon  changed temporarily
+REPLAY_MEMORY = 50000 # number of previous transitions to remember
+BATCH = 32 # size of minibatch
+FRAME_PER_ACTION = 1
 
 
 def weight_variable(shape):
@@ -66,3 +75,51 @@ def createNetwork():
     readout = tensorflow.matmul(h_fc1, W_fc2) + b_fc2
 
     return s, readout, h_fc1
+
+def trainNetwork(s, readout, h_fc1, sess):
+    # define the cost function
+    a = tf.placeholder("float", [None, ACTIONS])
+    y = tf.placeholder("float", [None])
+    readout_action = tf.reduce_sum(tf.multiply(readout, a), reduction_indices=1)
+    cost = tf.reduce_mean(tf.square(y - readout_action))
+    train_step = tf.train.AdamOptimizer(1e-6).minimize(cost)
+
+    # open up a game state to communicate with emulator
+    game_state = game.GameState()  # not sure if GameState is a function in game (MLModifiedSpaceShooter) though
+
+    # store the previous observations in replay memory
+    D = deque()
+
+    # printing
+    a_file = open("logs_" + GAME + "/readout.txt", 'w')
+    h_file = open("logs_" + GAME + "/hidden.txt", 'w')
+
+    # get the first state by doing nothing and preprocess the image to 80x80x4
+    do_nothing = np.zeros(ACTIONS)
+    do_nothing[0] = 1
+    x_t, r_0, terminal = game_state.frame_step(do_nothing)
+    x_t = cv2.cvtColor(cv2.resize(x_t, (80, 80)), cv2.COLOR_BGR2GRAY)
+    ret, x_t = cv2.threshold(x_t,1,255,cv2.THRESH_BINARY)
+    s_t = np.stack((x_t, x_t, x_t, x_t), axis=2)
+
+    # saving and loading networks
+    saver = tf.train.Saver()
+    sess.run(tf.initialize_all_variables())
+    checkpoint = tf.train.get_checkpoint_state("saved_networks")
+    if checkpoint and checkpoint.model_checkpoint_path:
+        saver.restore(sess, checkpoint.model_checkpoint_path)
+        print("Successfully loaded:", checkpoint.model_checkpoint_path)
+    else:
+        print("Could not find old network weights")
+
+
+def playGame():
+    sess = tf.InteractiveSession()
+    s, readout, h_fc1 = createNetwork()
+    trainNetwork(s, readout, h_fc1, sess)
+
+def main():
+    playGame()
+
+if __name__ == "__main__":
+    main()
